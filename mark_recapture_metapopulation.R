282ad0e31a00b4446a281b2bd8ea0ce19e0d1d4e
jensculrich <jensj27@gmail.com>
2022-05-25 15:12:41 -0700

    added new rfile for population size estimation in the metapopulation

---
+++ mark_recapture_metapopulation.R
@@ -0,0 +1,7 @@
+library(rstan)
+library(bayesplot)
+
+#### mark_recapture to estimate population sizes in a meta population
+#### with a site covariate (plant community/management)
+
+# JCU May, 2022
